a581d6967536b1177270f4c5c074cf5ed52f219a
Gregory Ostermayr <gregory.ostermayr@gmail.com>
2018-12-30 18:38:08 -0500

    move require

--- spec/graphml2json_spec.rb
+++ spec/graphml2json_spec.rb
@@ -1,5 +1,4 @@
 require 'spec_helper'
-require 'graphml2json'
 
 describe Graphml2Json do
   let(:json) {'{"nodes":[{"name":0},{"name":1},{"name":2},{"name":3}],"edges":[{"source":0,"target":1},{"source":1,"target":2},{"source":2,"target":3},{"source":3,"target":null}]}'}
