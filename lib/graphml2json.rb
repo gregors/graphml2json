c2eddee915c100d9ea042dec0c09ee545a612bd6
Gregory Ostermayr <gregory.ostermayr@gmail.com>
2018-12-30 18:45:17 -0500

    add data nodes as attributes in json

--- lib/graphml2json.rb
+++ lib/graphml2json.rb
@@ -3,7 +3,7 @@ require 'json'
 
 module Graphml2Json
 
-  def self.generate(xml_string) 
+  def self.generate(xml_string)
     doc = Nokogiri::XML(xml_string)
     doc.remove_namespaces!
     graph = doc.xpath('//graph')
@@ -14,21 +14,38 @@ module Graphml2Json
     @json_nodes = []
 
     @graph = {}
-    @graph[:nodes] = [] 
-    @graph[:edges] = [] 
+    @graph[:nodes] = []
+    @graph[:edges] = []
 
     nodes.each_with_index do |n, index|
-      @mapping[n[:id]] = index 
-      @graph[:nodes] << {:name => index}
+
+      attributes = data_attributes(n)
+      @mapping[n[:id]] = index
+      new_node = {:name => index}.merge(attributes)
+      @graph[:nodes] << new_node
     end
 
-    @json_edges = [] 
+    @json_edges = []
     edges.each do |e|
       source = @mapping[e[:source]]
       target = @mapping[e[:target]]
-      @graph[:edges] << { :source => source, :target => target }
+      attributes = data_attributes(e)
+      new_edge = { :source => source, :target => target }.merge(attributes)
+      @graph[:edges] << new_edge
     end
 
     JSON.generate(@graph)
+  end
+
+  private
+
+  def self.data_attributes(node)
+    data_nodes = node.xpath('./data')
+
+    attributes = data_nodes.map do |d|
+      {d[:key] => d.text}
+    end.inject(&:merge)
+
+    attributes || {}
   end
 end
--- spec/graphml2json_spec.rb
+++ spec/graphml2json_spec.rb
@@ -3,20 +3,28 @@ require 'spec_helper'
 describe Graphml2Json do
   let(:json) {'{"nodes":[{"name":0},{"name":1},{"name":2},{"name":3}],"edges":[{"source":0,"target":1},{"source":1,"target":2},{"source":2,"target":3},{"source":3,"target":null}]}'}
 
-  describe ".generate" do
-    context "missing proper namespace" do
+  describe '.generate' do
+    context 'missing proper namespace' do
       it 'returns valid json' do
-        graphml = File.read("spec/fixtures/graphml_without_namespacing.txt")
+        graphml = File.read('spec/fixtures/graphml_without_namespacing.txt')
         Graphml2Json::generate(graphml).should == json
       end
     end
 
-    context "with namespace" do
+    context 'with namespace' do
       it 'returns valid json' do
-        graphml = File.read("spec/fixtures/graphml_with_namespacing.txt")
+        graphml = File.read('spec/fixtures/graphml_with_namespacing.txt')
+        Graphml2Json::generate(graphml).should == json
+      end
+    end
+
+    context 'with nodes' do
+      let(:json){'{"nodes":[{"name":0,"color":"green"},{"name":1},{"name":2,"color":"blue"},{"name":3,"color":"red"},{"name":4},{"name":5,"color":"turquoise"}],"edges":[{"source":0,"target":2,"size":"1.0"},{"source":0,"target":1,"size":"1.0"},{"source":1,"target":3,"size":"2.0"},{"source":3,"target":2},{"source":2,"target":4},{"source":3,"target":5},{"source":5,"target":4,"size":"1.1"}]}'}
+
+      it 'returns data as attributes' do
+        graphml = File.read('spec/fixtures/attributes.graphml')
         Graphml2Json::generate(graphml).should == json
       end
     end
   end
 end
-
